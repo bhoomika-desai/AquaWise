f875b66e55f76475cdfd742964bbbd1404086f1c
LEKHAVISHWANATH1EW22EE013 <lekhagowda2004@gmail.com>
2026-03-02 23:24:24 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+import pickle
 
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import KNNImputer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
